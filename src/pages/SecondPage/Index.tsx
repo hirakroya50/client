f8b570d450482700414ba4b2c8dd9530776cc83b
hirakroya50 <hirakroya50@gmail.com>
2025-01-29 22:52:27 +0530

    working on the 2nd  page

--- src/pages/SecondPage/Index.tsx
+++ src/pages/SecondPage/Index.tsx
@@ -1,10 +1,12 @@
+import { Button } from "@/components/ui/button";
 import ImageSlider from "./_sub-components/ImageSlider";
 import QuestionAnswer from "./_sub-components/QuestionAnswer";
 import VideoCardContainer from "./_sub-components/VideoCardContainer";
 
 const SecondPage = () => {
   return (
-    <div className="px-8">
+    <div className="">
+      <HeaderCompponent />
       <ImageSlider />
       <VideoCardContainer />
       <QuestionAnswer />
@@ -13,3 +15,19 @@ const SecondPage = () => {
 };
 
 export default SecondPage;
+
+const HeaderCompponent = () => {
+  return (
+    <div className="fixed top-0 z-10 flex justify-between w-full border text-white bg-gray-900 ">
+      <p>CopyBoard By Jser</p>
+      <div>
+        <Button className="transition-all select-none h-10 justify-center rounded-xl text-sm font-medium bg-gray-700 text-gray-200 hover:bg-gray-600 py-3 px-5 flex items-center gap-2 pl-5 pr-[18px]">
+          API
+        </Button>
+        <Button>Jsper Free Trial</Button>
+        <Button>Pricing</Button>
+        <Button>Sign-in / Sign-up</Button>
+      </div>
+    </div>
+  );
+};
--- src/pages/SecondPage/_sub-components/ImageSlider.tsx
+++ src/pages/SecondPage/_sub-components/ImageSlider.tsx
@@ -15,79 +15,77 @@ const ImageSlider = () => {
   ];
   console.log(imageArray);
   return (
-    <div>
-      <div className="bg-black">
-        <div className="relative w-full force-full-screen-w m-auto second-page-dot-background flex flex-col items-center justify-center gap-8 py-8 md:py-16">
-          <input
-            type="range"
-            step="0.01"
-            min="0"
-            max="100"
-            value={value}
-            onChange={handleChange}
-          />
-          <p>Current Value: {value}</p>
-          <div className="image border border-red-500 h-full w-full flex justify-center overflow-hidden flex-1">
+    <div className="bg-black border border-red-700 mt-10">
+      <div className=" w-full force-full-screen-w m-auto second-page-dot-background flex flex-col items-center justify-center gap-8 py-8 md:py-16">
+        <input
+          type="range"
+          step="0.01"
+          min="0"
+          max="100"
+          value={value}
+          onChange={handleChange}
+        />
+        <p>Current Value: {value}</p>
+        <div className="image border border-red-500 h-full w-full flex justify-center overflow-hidden flex-1">
+          <div
+            className="relative overflow-hidden max-h-full h-auto w-auto"
+            style={{
+              aspectRatio: "1/1",
+            }}
+          >
             <div
-              className="relative overflow-hidden max-h-full h-auto w-auto"
+              className="relative overflow-hidden flex justify-center items-center m-auto rounded-2xl max-h-full h-auto"
               style={{
                 aspectRatio: "1/1",
               }}
             >
               <div
-                className="relative overflow-hidden flex justify-center items-center m-auto rounded-2xl max-h-full h-auto"
+                className="relative rounded-xl overflow-hidden h-full"
                 style={{
                   aspectRatio: "1/1",
                 }}
               >
-                <div
-                  className="relative rounded-xl overflow-hidden h-full"
-                  style={{
-                    aspectRatio: "1/1",
-                  }}
-                >
-                  <div className="h-full">
-                    <img
-                      src="/Portrait-Relight.webp"
-                      alt=""
-                      className="max-w-full  pointer-events-none m-auto h-full"
-                      style={{
-                        aspectRatio: "1/1",
-                      }}
-                    />
-                  </div>
-
-                  <div
-                    className="absolute top-0 left-0 h-full w-full"
+                <div className="h-full">
+                  <img
+                    src="/Portrait-Relight.webp"
+                    alt=""
+                    className="max-w-full  pointer-events-none m-auto h-full"
                     style={{
-                      clipPath: `polygon(${value}% 0px, 100% 0px, 100% 100%, ${value}% 100%)`,
+                      aspectRatio: "1/1",
                     }}
-                  >
-                    <img
-                      src="/Portrait.webp"
-                      className="max-w-full  pointer-events-none m-auto bg-gray-200 dark:bg-gray-800 h-full"
-                      alt=""
-                    />
-                  </div>
-
-                  {/* slider path */}
-                  <div
-                    className="absolute w-0.5 bg-gray-100 h-full top-0"
-                    style={{
-                      left: `calc(${value}% - 1px)`,
-                    }}
-                  ></div>
-
-                  <input
-                    className="image-comparison-slider"
-                    type="range"
-                    step="0.01"
-                    min="0"
-                    max="100"
-                    value={value}
-                    onChange={handleChange}
                   />
                 </div>
+
+                <div
+                  className="absolute top-0 left-0 h-full w-full"
+                  style={{
+                    clipPath: `polygon(${value}% 0px, 100% 0px, 100% 100%, ${value}% 100%)`,
+                  }}
+                >
+                  <img
+                    src="/Portrait.webp"
+                    className="max-w-full  pointer-events-none m-auto bg-gray-200 dark:bg-gray-800 h-full"
+                    alt=""
+                  />
+                </div>
+
+                {/* slider path */}
+                <div
+                  className="absolute w-0.5 bg-gray-100 h-full top-0"
+                  style={{
+                    left: `calc(${value}% - 1px)`,
+                  }}
+                ></div>
+
+                <input
+                  className="image-comparison-slider"
+                  type="range"
+                  step="0.01"
+                  min="0"
+                  max="100"
+                  value={value}
+                  onChange={handleChange}
+                />
               </div>
             </div>
           </div>
